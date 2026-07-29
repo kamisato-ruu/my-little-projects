18bd9bde6892d78c9b2d1890a519d84049cd1579
kamisato-ruu <233084083+kamisato-ruu@users.noreply.github.com>
2026-07-29 17:07:57 +0700

    2

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -6,14 +6,17 @@ This Python script calculates Body Mass Index (BMI) based on a person's height a
 """
 #BMI = BB / (TB(cm) ** 2) * 10000
 
-
+#THE TITLE OF CALCULAOTR
 print("THIS IS BMI CALCULATOR")
 
+#user input thier height and weight
 height =  int(input("Input your height in kg (e.g 190) : "))
 weight = int(input("Inpur your wight in cm (e.g 80) : "))
 
+#formula of bmi calculator
 bmi_result = float(weight / (height ** 2 )) * 10000
 
+#bmi's category
 if bmi_result < 18.5:
     categories = "Under Weight"
 elif bmi_result < 25:
@@ -25,5 +28,6 @@ elif bmi_result < 40:
 else:
     categories = "Severe Obesity"
 
+#show output for user
 print(f"Your BMI is {bmi_result:.2f}.")
 print(f"You categorized by {categories}.")
