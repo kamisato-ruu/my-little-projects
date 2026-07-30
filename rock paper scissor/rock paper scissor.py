7f0a5dc2f7af1257a829e15127e402166599bf62
kamisato-ruu <233084083+kamisato-ruu@users.noreply.github.com>
2026-07-31 00:07:22 +0700

    new

--- rock paper scissor/rock paper scissor.py
+++ rock paper scissor/rock paper scissor.py
@@ -1,6 +1,15 @@
 import random
 
 computer = random.choice(['rock', 'paper', 'scissor'])
+beats = {'rock' : 'scissor', 'paper' : ' rock', 'scissor' : 'paper' }
+
 player = input("Pick rock, paper or scissor : ").strip().lower()
 
-print(f"Computer use {computer}")
+print(f"You pick {player} computer use {computer}")
+
+if player == computer:
+    print('itss drawww')
+elif beats[player] == computer:
+    print('you win!!')
+else:
+    print('noo you losee!!')
---
+++ rock paper scissor/rtes.py
@@ -0,0 +1,5 @@
+import random
+
+dictionary = {'Satu' : 'Dua', 'TIga' : 'Empat', 'Lima' : 'Enam'}
+acak = random.choice(['helo', 'gatau', 'esteh'])
+print(dictionary[acak])
