26d1552c844944bff679f636dfa319608bcb1e70
kamisato-ruu <233084083+kamisato-ruu@users.noreply.github.com>
2026-07-31 08:18:36 +0700

    yey

--- simple calculator/simple calculator.py
+++ simple calculator/simple calculator.py
@@ -1,21 +1,61 @@
-print('SELAMAT DATANG DI KALKULATOR SEDERHANA\n')
+print('\nSELAMAT DATANG DI KALKULATOR SEDERHANA\n')
 print('Apa yang ingin kamu lakukan....')
-print('Masukkan angka yang sesuai untuk melakukan operasi!')
-print('Masukkan angka 1 untuk penjumlahan')
-print('Masukkan angka 2 untuk pengurangan')
-print('Masukkan angka 3 untuk perkalian')
-print('Masukkan angka 4 untuk pembagian\n')
-
-operasi = int(input('Masukkan angka untuk memilih operasi yang akan dilakukan : '))
-
-if operasi == 1:
-    print('Kamu memilih operasi penjumlahan!\n')
-    def tambah(a, b):
-        hasil = a + b
-        return hasil
-    a = float(input('Masukkan angka : '))
-    b = float(input('Masukkan angka : '))
-
-    penjumlahan1 = tambah(a, b)
-    print(f'\nHasil penjumlahan adalah {penjumlahan1}')
+def instruksi():
+    print('Masukkan angka yang sesuai untuk melakukan operasi!')
+    print('Masukkan angka 1 untuk penjumlahan')
+    print('Masukkan angka 2 untuk pengurangan')
+    print('Masukkan angka 3 untuk perkalian')
+    print('Masukkan angka 4 untuk pembagian')
+    print('Masukkan angka -1 untuk keluar dari program\n')
     
+def tambah(a, b):
+    hasil = a + b
+    return hasil
+def kurang(a, b):
+    hasil = a - b
+    return hasil
+def kali(a,b):
+    hasil = a * b
+    return hasil
+def bagi(a, b):
+    hasil = a / b
+    return hasil
+
+while True:
+    instruksi()
+    operasi = int(input('Masukkan angka untuk memilih operasi yang akan dilakukan : '))
+    if operasi == 1:
+        print('Kamu memilih operasi penjumlahan!\n')
+        a = float(input('Masukkan angka : '))
+        b = float(input('Masukkan angka : '))
+
+        penjumlahan1 = tambah(a, b)
+        print(f'\nHasil penjumlahan adalah {penjumlahan1}.\n')
+    elif operasi == 2:
+        print('Kamu memilih operasi pengurangan!\n')
+        a = float(input('Masukkan angka : '))
+        b = float(input('Masukkan angka : '))
+
+        penngurangan1 = kurang(a, b)
+        print(f'\nHasil penjumlahan adalah {penngurangan1}.\n')
+    elif operasi == 3:
+        print('Kamu memilih operasi perkalian!\n')
+        a = float(input('Masukkan angka : '))
+        b = float(input('Masukkan angka : '))
+
+        perkalian1 = kali(a, b)
+        print(f'\nHasil penjumlahan adalah {perkalian1}.\n')
+    elif operasi == 4:
+        print('Kamu memilih operasi pembagian!\n')
+        a = float(input('Masukkan angka : '))
+        b = float(input('Masukkan angka : '))
+
+        pembagian1 = bagi(a, b)
+        print(f'\nHasil penjumlahan adalah {pembagian1}.\n')
+    elif operasi == -1:
+        print('Terima kasih sudah menggunakan program kalkulator sederhana kami.\n')
+        break
+    else:
+        print('\nANGKA YANG ANDA MASUKKAN TIDAK SESUAI!!\n')
+        
+        
