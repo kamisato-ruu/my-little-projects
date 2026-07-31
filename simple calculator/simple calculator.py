45fe55ba8145f164b1385e5724ff9b1dc874ca67
kamisato-ruu <233084083+kamisato-ruu@users.noreply.github.com>
2026-07-31 14:31:18 +0700

    ya

--- simple calculator/simple calculator.py
+++ simple calculator/simple calculator.py
@@ -21,40 +21,51 @@ def bagi(a, b):
     hasil = a / b
     return hasil
 
+def masukkan():
+    a = float(input('Masukkan angka : ').replace(',', '.'))
+    b = float(input('Masukkan angka : ').replace(',', '.'))
+    return a, b
+
+
 while True:
     instruksi()
-    operasi = int(input('Masukkan angka untuk memilih operasi yang akan dilakukan : '))
+    try:
+        operasi = int(input('Masukkan angka untuk memilih operasi yang akan dilakukan : '))
+    except ValueError:
+        print("Error, hanya bisa memasukkan angka!!\n")
+        continue
+
     if operasi == 1:
         print('Kamu memilih operasi penjumlahan!\n')
-        a = float(input('Masukkan angka : ').replace(',', '.'))
-        b = float(input('Masukkan angka : ').replace(',', '.'))
-
+        a, b = masukkan()
         penjumlahan1 = tambah(a, b)
         print(f'\nHasil penjumlahan adalah {penjumlahan1:g}.\n')
+
     elif operasi == 2:
         print('Kamu memilih operasi pengurangan!\n')
-        a = float(input('Masukkan angka : ').replace(',', '.'))
-        b = float(input('Masukkan angka : ').replace(',', '.'))
-
+        a, b = masukkan()
         penngurangan1 = kurang(a, b)
         print(f'\nHasil pengurangan adalah {penngurangan1:g}.\n')
+
     elif operasi == 3:
         print('Kamu memilih operasi perkalian!\n')
-        a = float(input('Masukkan angka : ').replace(',', '.'))
-        b = float(input('Masukkan angka : ').replace(',', '.'))
-
+        a, b = masukkan()
         perkalian1 = kali(a, b)
         print(f'\nHasil perkalian adalah {perkalian1:g}.\n')
+
     elif operasi == 4:
         print('Kamu memilih operasi pembagian!\n')
-        a = float(input('Masukkan angka : ').replace(',', '.'))
-        b = float(input('Masukkan angka : ').replace(',', '.'))
+        a, b = masukkan()
+        if b == 0:
+            print('Tidak bisa melakukan pembagian dengan 0.\n')
+        else:
+            pembagian1 = bagi(a, b)
+            print(f'\nHasil pembagian adalah {pembagian1:g}.\n')
 
-        pembagian1 = bagi(a, b)
-        print(f'\nHasil pembagian adalah {pembagian1:g}.\n')
     elif operasi == -1:
         print('Terima kasih sudah menggunakan program kalkulator sederhana kami.\n')
         break
+    
     else:
         print('\nANGKA YANG ANDA MASUKKAN TIDAK SESUAI!!\n')
         
