e1c88cea53c4d281af4ad7e63a25de972c66e9e0
kamisato-ruu <233084083+kamisato-ruu@users.noreply.github.com>
2026-09-15 17:16:21 +0700

    make the button clickable

--- calculator_gui/script/script.js
+++ calculator_gui/script/script.js
@@ -1,16 +1,25 @@
 const numbers = document.querySelectorAll(".number");
 const display = document.getElementById("output-display-main-screen");
 const operations = document.querySelectorAll("[data-operators");
+const deleteButton = document.getElementById('delete');
+const clearButton = document.getElementById('clear')
 
-numbers
-  .forEach(function (number) {
-    number.addEventListener("click", function () {
-      display.value += number.textContent;
-    });
-  })
+numbers.forEach(function (number) {
+  number.addEventListener("click", function () {
+    display.value += number.textContent;
+  });
+});
 
-  operations.forEach(function(operator) {
-    operator.addEventListener("click", function() {
-        display.value += operator.dataset.operators
-    })
-  })
+operations.forEach(function (operator) {
+  operator.addEventListener("click", function () {
+    display.value += operator.dataset.operators;
+  });
+});
+
+deleteButton.addEventListener('click', function() {
+    display.value = display.value.slice(0,-1);
+})
+
+clearButton.addEventListener('click', function() {
+    display.value='';
+})
